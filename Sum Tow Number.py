a23f266ec0da02bdd5cbf8f63a43be7a82f38665
Abhishek <ak724591@gmail.com>
2021-03-19 20:06:38 -0700

    version 2

--- Sum Tow Number.py
+++ Sum Tow Number.py
@@ -1,8 +1,8 @@
-# TEST 1
+
 print("Enter first number")
 var1 = input()
 
 print("Enter second number")
 var2 = input()
 
-print("Sum of your tow entered number is ", int(var1)+int(var2))
+print("Sum of your tow entered number is =>", int(var1)+int(var2))
